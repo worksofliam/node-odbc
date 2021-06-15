0ad018268dd474cd686809b45f94c651202fad8c
Liam Allan <liam@worksofbarry.com>
2021-06-15 10:15:49 -0400

    Add callProcedure to pool class

---
+++ test/pool/callProcedure.js
@@ -0,0 +1,43 @@
+/* eslint-env node, mocha */
+
+require('dotenv').config();
+const assert = require('assert');
+const odbc = require('../../');
+
+describe('.callProcedure(procedureName, parameters, [callback])...', () => {
+  describe('...with callbacks...', () => {
+    it('...should place correct result in an out parameter.', (done) => {
+      const array = [undefined];
+      odbc.connect(`${process.env.CONNECTION_STRING}`, (error1, pool) => {
+        assert.deepEqual(error1, null);
+        pool.callProcedure(null, `${process.env.DB_SCHEMA}`, `${process.env.DB_STOREDPROCEDURE}`, array, (error2, result2) => {
+          assert.deepEqual(error2, null);
+          assert.notDeepEqual(result2, null);
+          done();
+        });
+      });
+    });
+    it('...should place correct result in an multiple out parameters.', (done) => {
+      const array = [undefined, undefined];
+      odbc.pool(`${process.env.CONNECTION_STRING}`, (error1, pool) => {
+        assert.deepEqual(error1, null);
+        pool.callProcedure(null, `${process.env.DB_SCHEMA}`, `${process.env.DB_STOREDPROCEDURE2}`, array, (error2, result2) => {
+          assert.deepEqual(error2, null);
+          assert.notDeepEqual(result2, null);
+          done();
+        });
+      });
+    });
+    it.skip('...should return SQL_LONGVARCHAR data.', (done) => {
+      const array = [undefined];
+      odbc.pool(`${process.env.CONNECTION_STRING}`, (error1, pool) => {
+        assert.deepEqual(error1, null);
+        pool.callProcedure(null, 'MIRISH', 'CLOBPROC', array, (error2, result2) => {
+          assert.deepEqual(error2, null);
+          assert.notDeepEqual(result2, null);
+          done();
+        });
+      });
+    });
+  });
+});
